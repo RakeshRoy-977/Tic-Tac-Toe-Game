5809183332865eced9f9acfa952b97a0e395e96c
RakeshRoy-977 <15rakeshroy@gmail.com>
2023-11-26 20:44:54 +0530

    fix bug

--- src/Page/Game.jsx
+++ src/Page/Game.jsx
@@ -22,20 +22,20 @@ const Game = () => {
     <>
     <div className="Box">
         <div className="columns">
-            <div onClick={(x)=>clicked(0)} className="row"><span>{index[0]}</span></div>
-            <div onClick={(x)=>clicked(1)} className="row"><span>{index[1]}</span></div>
-            <div onClick={(x)=>clicked(2)} className="row"><span>{index[2]}</span></div>
+            <div onClick={()=>clicked(0)} className="row"><span>{index[0]}</span></div>
+            <div onClick={()=>clicked(1)} className="row"><span>{index[1]}</span></div>
+            <div onClick={()=>clicked(2)} className="row"><span>{index[2]}</span></div>
             
         </div>
         <div className="columns">
-            <div onClick={(x)=>clicked(3)} className="row"><span>{index[3]}</span></div>
-            <div onClick={(x)=>clicked(4)} className="row"><span>{index[4]}</span></div>
-            <div onClick={(x)=>clicked(5)} className="row"><span>{index[5]}</span></div>
+            <div onClick={()=>clicked(3)} className="row"><span>{index[3]}</span></div>
+            <div onClick={()=>clicked(4)} className="row"><span>{index[4]}</span></div>
+            <div onClick={()=>clicked(5)} className="row"><span>{index[5]}</span></div>
         </div>
         <div className="columns">
-            <div onClick={(x)=>clicked(6)} className="row"><span>{index[6]}</span></div>
-            <div onClick={(x)=>clicked(7)} className="row"><span>{index[7]}</span></div>
-            <div onClick={(x)=>clicked(8)} className="row"><span>{index[8]}</span></div>
+            <div onClick={()=>clicked(6)} className="row"><span>{index[6]}</span></div>
+            <div onClick={()=>clicked(7)} className="row"><span>{index[7]}</span></div>
+            <div onClick={()=>clicked(8)} className="row"><span>{index[8]}</span></div>
         </div> 
     </div>
     <div>
